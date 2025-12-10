4ba1ff1de77e7d9fbc14ff516452c779249b2445
kevin Heifner <kevin@wire.network>
2025-12-10 09:15:03 -0600

    Do not oc interrupt during replay.
    (cherry picked from commit 96bcb7544e3ac92fb7696cfed9c9052a4ed02af3)

--- libraries/chain/include/eosio/chain/controller.hpp
+++ libraries/chain/include/eosio/chain/controller.hpp
@@ -282,6 +282,8 @@ namespace eosio::chain {
          void   set_key_blacklist( const flat_set<public_key_type>& );
 
          void   set_disable_replay_opts( bool v );
+         // thread safe
+         bool   is_replaying() const;
 
          block_handle         head()const;
          block_handle         fork_db_head()const;
